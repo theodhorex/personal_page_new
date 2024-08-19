605fbfad9132766fd50494977db577b43c9118ed
theodhorex <theodhore76@gmail.com>
2024-08-20 00:59:44 +0700

    almost there!

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup></script>
 
 <template>
-  <main class="">
+  <main class="relative">
     <div class="bg-gray-200 rounded-bl-[5.5rem] rounded-br-[5.5rem]">
       <div class="p-12 grid grid-cols-2">
         <div>
@@ -23,11 +23,65 @@
             Creating, Building, & Deploy <br />
             Your Website
           </h1>
-          <h1 class="w-[80%] font-normal mt-6">
+          <h1 class="w-[80%] font-normal mt-6 mb-6">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
             incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
           </h1>
-          
+          <a href="#" class="bg-white font-semibold text-sm px-6 py-2 rounded-full"
+            >Learn More</a
+          >
+        </div>
+      </div>
+    </div>
+
+    <div>
+      <h1 class="text-center font-semibold my-10">My Toolbox: Technoliges I Work With</h1>
+      <div class="techImage flex items-center justify-center">
+        <img class="mx-10 mb-14" src="./assets/img/image 1.png" width="60" alt="" />
+        <img class="mx-10 mb-14" src="./assets/img/image 2.png" width="69" alt="" />
+        <img class="mx-10 mb-14" src="./assets/img/image 3.png" width="88" alt="" />
+        <img class="mx-10 mb-14" src="./assets/img/image 4.png" width="66" alt="" />
+        <img class="mx-10 mb-14" src="./assets/img/image 5.png" width="94" alt="" />
+        <img class="mx-10 mb-14" src="./assets/img/image 6.png" width="72" alt="" />
+      </div>
+    </div>
+
+    <div class="bg-gray-200 py-24 rounded-[5.5rem]">
+      <h1 class="font-semibold text-4xl w-[50%] text-center block mx-auto">
+        Crafting solutions with code, fueled by imagination.
+      </h1>
+      <div class="grid grid-cols-4 gap-10 border-t-[1px] border-gray-400 pt-14 mt-20 w-[80%] block mx-auto">
+        <div>
+          <img class="mb-4" src="./assets/img/image 4.png" width="35" alt="" />
+          <h1 class="font-semibold text-sm w-[45%]">Full-Stack Web Development</h1>
+          <h1 class="text-xs mt-3">
+            Building robust and scalable web application from front-end to back-end,
+            ensuring a seamless user experience and high peformance.
+          </h1>
+        </div>
+        <div>
+          <img class="mb-4" src="./assets/img/image 4.png" width="35" alt="" />
+          <h1 class="font-semibold text-sm">UI/UX Design</h1>
+          <h1 class="text-xs mt-3">
+            Designing intuitive and visually appealing interfaces that prioritize user
+            experience, making websites not only functional but also enjoyable to use.
+          </h1>
+        </div>
+        <div>
+          <img class="mb-4" src="./assets/img/image 4.png" width="35" alt="" />
+          <h1 class="font-semibold text-sm">Web Based Application</h1>
+          <h1 class="text-xs mt-3">
+            Creating responsive and dynamic web applications tailored to meet your
+            business needs, ensuring cross-platform compability and user-centric design.
+          </h1>
+        </div>
+        <div>
+          <img class="mb-4" src="./assets/img/image 4.png" width="35" alt="" />
+          <h1 class="font-semibold text-sm">Responsive Design</h1>
+          <h1 class="text-xs mt-3">
+            Crafting websites that look great and perform flawlessly on any device, from
+            desktops to mobile phones, ensuring accessbility and ease to use.
+          </h1>
         </div>
       </div>
     </div>

53d38978aa8090f51bbb319c38ed908c87c65078
theodhorex <theodhore76@gmail.com>
2024-08-19 22:14:50 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script setup></script>
+
+<template>
+  <main class="">
+    <div class="bg-gray-200 rounded-bl-[5.5rem] rounded-br-[5.5rem]">
+      <div class="p-12 grid grid-cols-2">
+        <div>
+          <h1 class="font-medium text-xs">aurelio.theodhore@gmail.com</h1>
+        </div>
+        <div>
+          <ul class="flex float-end">
+            <li class="mr-2 font-semibold text-xs">About Me</li>
+            <li class="mr-2 font-semibold text-xs">/</li>
+            <li class="mr-2 font-semibold text-xs">Projects</li>
+            <li class="mr-2 font-semibold text-xs">/</li>
+            <li class="mr-2 font-semibold text-xs">FaQs</li>
+          </ul>
+        </div>
+      </div>
+      <div class="p-14 px-13 grid grid-cols-2">
+        <div>
+          <h1 class="text-3xl font-bold">
+            Creating, Building, & Deploy <br />
+            Your Website
+          </h1>
+          <h1 class="w-[80%] font-normal mt-6">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.
+          </h1>
+          
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
